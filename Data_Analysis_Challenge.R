6bbe5527db649d4dd12fdc1e655d08d1d93dc7c0
MadhuCJ <74353910+MadhuCJ@users.noreply.github.com>
2020-11-17 23:00:37 +0530

    Update Data_Analysis_Challenge.R

--- Data_Analysis_Challenge.R
+++ Data_Analysis_Challenge.R
@@ -1,8 +1,11 @@
-Data_Analysis_Challenge_Data_Set
+#Data_Analysis_Challenge_Data_Set
+#Data is read
 library(readr)
 Data_Analysis_Challenge_Data_Set <- read_csv("C:/Users/Prakash/Desktop/Data Analysis Challenge Data Set.csv")
  View(Data_Analysis_Challenge_Data_Set)
+#attached the file to use the data present in the file
  attach(Data_Analysis_Challenge_Data_Set)
+#Statistical data
 summary(`wheel-base`)
  summary(length)
  summary(width)
@@ -16,6 +19,7 @@ summary(`wheel-base`)
  summary(`peak-rpm`)
  summary(`city-mpg`)
  summary(`highway-mpg`)
+#Standard Deviation
  sd(`wheel-base`)
  sd(length)
  sd(width)
@@ -25,6 +29,7 @@ summary(`wheel-base`)
  sd(`compression-ratio`)
  sd(`city-mpg`)
  sd(`highway-mpg`)
+#Two dimensional Analysis
  hist(Data_Analysis_Challenge_Data_Set$`wheel-base`)
  hist(Data_Analysis_Challenge_Data_Set$length)
  hist(Data_Analysis_Challenge_Data_Set$width)
@@ -35,24 +40,35 @@ summary(`wheel-base`)
  hist(Data_Analysis_Challenge_Data_Set$`city-mpg`)
  hist(Data_Analysis_Challenge_Data_Set$`highway-mpg`)
  hist(Data_Analysis_Challenge_Data_Set$`compression-ratio`)
+#Panel data Analysis
  library(plm)
  mydata<-read.csv("C:/Users/Prakash/Desktop/Data Analysis Challenge Data Set.csv")
  attach(mydata)
+#X parameters
  X<-cbind(length,width,height)
+#Creation of panel data
  pdata<-plm.data(mydata, index=c("symboling"))
+#Y parameters
  Y<- cbind(`curb-weight`)
+#Summary of the dependant and independant variables.
  summary(Y)
  summary(X)
+#Pooling Method
  pooling<-plm(Y~X,data=pdata,model="pooling")
  summary(pooling)
+#Between Estimator
  between<-plm(Y~X,data=pdata,model="between")
  summary(between)
+#First Difference Estimator
  FirstDifference<-plm(Y~X,data=pdata,model="fd")
  summary(FirstDifference)
+#Within Estimator
  WithinEstimator<-plm(Y~X,data=pdata,model="within")
  summary(WithinEstimator)
+#Random Effects Estimator
  RandomEffects<-plm(Y~X,data=pdata,model="random")
  summary(RandomEffects)
+#LM Test
  plmtest(pooling)
  pFtest(FirstDifference,pooling)
  phtest(RandomEffects,FirstDifference)
